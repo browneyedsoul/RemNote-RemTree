bed564a90aa7ae168595310b9991c6e0f2bc3ae0
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-02-23 13:42:39 +0900

    Update rem focus view

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -2,20 +2,29 @@ import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
 
 export const [REMTREE_POWERUP, REMTREEC_POWERUP] = ["remtree_powerup", "remtreec_powerup"];
 
-let TreeCSS = "";
-
-fetch("snippet.css")
-  .then((response) => response.text())
-  .then((text) => {
-    // Use the entire contents of the CSS file
-    TreeCSS = text;
-
-    // Use the extracted text in your JavaScript code
-    console.log(TreeCSS);
-  })
-  .catch((error) => console.error(error));
+let TreeCSS: string;
 
 async function onActivate(plugin: ReactRNPlugin) {
+  try {
+    await fetch("snippet.css")
+      .then((response) => response.text())
+      .then((text) => {
+        TreeCSS = text;
+        console.log("Rem Tree Installed");
+      })
+      .catch((error) => console.error(error));
+  } catch (error) {
+    await fetch("https://raw.githubusercontent.com/browneyedsoul/RemNote-RemTree/main/src/snippet.css")
+      .then((response) => response.text())
+      .then((text) => {
+        TreeCSS = text;
+        console.log("Rem Tree Installed from cdn");
+      })
+      .catch((error) => console.error(error));
+  }
+  
+  await plugin.app.registerCSS("rem-tree", TreeCSS);
+  
   await plugin.app.registerPowerup("Tree", REMTREE_POWERUP, "A Power-up Block for decorating texts", { slots: [] });
   await plugin.app.registerPowerup("Treec", REMTREEC_POWERUP, "A Power-up Block for decorating texts", { slots: [] });
 
@@ -35,8 +44,6 @@ async function onActivate(plugin: ReactRNPlugin) {
       await rem?.addPowerup(REMTREEC_POWERUP);
     },
   });
-  
-  await plugin.app.registerCSS("rem-tree", TreeCSS);
 }
 
 async function onDeactivate(_: ReactRNPlugin) {}
